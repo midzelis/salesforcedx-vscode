da60d26fd2acba219a96f7252ee9c450081f51cf
Min Idzelis <midzelis@salesforce.com>
2019-01-21 11:34:56 -0500

    Add link-lsp

--- packages/salesforcedx-vscode-lwc/src/index.ts
+++ packages/salesforcedx-vscode-lwc/src/index.ts
@@ -111,7 +111,7 @@ function startLWCLanguageServer(
   serverModule: string,
   context: vscode.ExtensionContext
 ) {
-  const debugOptions = { execArgv: ['--nolazy', '--inspect-brk=6009'] };
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
   // If the extension is launched in debug mode then the debug server options are used
   // Otherwise the run options are used
   const serverOptions: ServerOptions = {
